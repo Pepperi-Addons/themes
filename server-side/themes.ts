cf36181167dd7481ac00409040c39d6ea24a84df
tomerp_pepperi <tomer.p@pepperi.com>
2021-11-08 15:25:30 +0200

    - Merge to version 12.

--- server-side/themes.ts
+++ server-side/themes.ts
@@ -1,7 +1,5 @@
 import MyService from './my.service';
-import JavascriptService from './my.javascript.service.js';
 import {Client, Request} from '@pepperi-addons/debug-server';
-import {InstalledAddon} from '@pepperi-addons/papi-sdk';
 
 // add functions here
 // this function will run on the 'api/foo' endpoint
@@ -10,12 +8,6 @@ export async function foo(client: Client, request: Request) {
     const service = new MyService(client);
     const res = await service.getAddons();
     return res;
-}
-
-// this function will run on api/js_foo endpoint
-// note that the code here is running from a javascript file
-export async function js_foo(client: Client, request: Request) {
-    return JavascriptService.js_foo(client, request);
 }
 
 export async function css_variables(client: Client, request: Request) {
